271401491b84ee04b69cd49d6dff5e9f700d69e5
mustafa <moshassen000@gmail.com>
2025-05-06 21:31:41 +0300

    -make Home screen API service class -make specialization response model -make Home repo

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -19,7 +19,7 @@ class DocApp extends StatelessWidget {
           scaffoldBackgroundColor: Colors.white,
         ),
         debugShowCheckedModeBanner: false,
-        initialRoute: Routes.homeScreen,
+        initialRoute: Routes.onBoardingScreen,
         onGenerateRoute: appRouter.generateRoute,
       ),
     );
